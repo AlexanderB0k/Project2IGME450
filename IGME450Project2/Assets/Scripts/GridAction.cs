d8579d038f2bde41836ff4b6a1b56014f2089978
SleepyGameZs <76854561+SleepyGameZs@users.noreply.github.com>
2025-03-24 10:23:37 -0400

    Pushing changes

---
+++ IGME450Project2/Assets/Scripts/GridAction.cs
@@ -0,0 +1,166 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Difficulty
+{
+    Starting,
+    Beginner,
+    Novice,
+    Advance,
+    Expert
+}
+
+public class GridAction : MonoBehaviour
+{ 
+
+    [SerializeField] private List<AttackPatterns> fullPatternList;
+
+    [SerializeField] private Difficulty _currentDifficulty;
+
+    [SerializeField] private AttackPatterns currentPattern;
+
+    private Dictionary<Difficulty , List<AttackPatterns>> PatternsRegistry = new Dictionary<Difficulty, List<AttackPatterns>>();
+
+    private List<AttackPatterns> selectedPatternList;
+
+    [Header("Timers")]
+
+    [SerializeField] private float globalTimer = 0f;
+
+    private float newPatterndelay = 1.0f;
+
+    private float patternLifeTimeTImer = 1.0f;
+
+    private void Start()
+    {
+        DefineDictionary();
+
+        
+    }
+
+    private void Update()
+    {
+        globalTimer += Time.deltaTime;
+
+        //Step 1 - determine the current difficulty based on the time
+        DetermineDiffuclty();
+
+        //Step 2 - get a random pattern based on the currenty difficulty
+        GetRandomPattern();
+
+        //Step 3 - changes the tile
+        ChangeGridPattern();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void ChangeGridPattern()
+    {
+        for (int x = 0; x < currentPattern.cols; x++)
+        {
+            string tempString = "";
+            for (int y = 0; y < currentPattern.rows; y++)
+            {
+                tempString += $" {currentPattern.tileGrid[x].row[y].isDangerous}";
+
+                if (currentPattern.tileGrid[x].row[y].isDangerous)
+                {
+                    GridManager.Instance.TileList[y][x].GetComponent<SpriteRenderer>().color = Color.red;
+                }
+
+            }
+
+            //Debug.Log(tempString);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void DetermineDiffuclty()
+    {
+        if (globalTimer < 1.0f)
+        {
+            //Debug.Log("starting");
+            _currentDifficulty = Difficulty.Starting;
+        }
+        else if (globalTimer >= 1.0f && globalTimer < 2.0f)
+        {
+            //Debug.Log("beginner");
+            _currentDifficulty = Difficulty.Beginner;
+        }
+        else if (globalTimer >= 2.0f && globalTimer < 3.0f)
+        {
+            //Debug.Log("novice");
+            _currentDifficulty = Difficulty.Novice;
+        }
+        else if (globalTimer >= 3.0f && globalTimer < 4.0f)
+        {
+            //Debug.Log("advance");
+            _currentDifficulty = Difficulty.Advance;
+        }
+        else if (globalTimer >= 7.0f)
+        {
+            //Debug.Log("expert");
+            _currentDifficulty = Difficulty.Expert;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void GetRandomPattern()
+    {
+        selectedPatternList = PatternsRegistry[_currentDifficulty];
+
+        Debug.Log(selectedPatternList.Count);
+
+        currentPattern = selectedPatternList[Random.Range(0, selectedPatternList.Count - 1)];
+    }
+
+    /// <summary>
+    /// This 
+    /// </summary>
+    private void DefineDictionary()
+    {
+        List<AttackPatterns> starting = new List<AttackPatterns>();
+
+        List<AttackPatterns> beginner = new List<AttackPatterns>();
+
+        List<AttackPatterns> novice = new List<AttackPatterns>();
+
+        List<AttackPatterns> advance = new List<AttackPatterns>();
+
+        List<AttackPatterns> expert = new List<AttackPatterns>();
+
+        //If we add another diffuclty change the the number 
+        for (int x = 0; x < fullPatternList.Count; x++)
+        {
+            switch (fullPatternList[x].DifficultyRating)
+            {
+                case Difficulty.Starting:
+                    starting.Add(fullPatternList[x]);
+                    break;
+                case Difficulty.Beginner:
+                    beginner.Add(fullPatternList[x]);
+                    break;
+                case Difficulty.Novice:
+                    novice.Add(fullPatternList[x]);
+                    break;
+                case Difficulty.Advance:
+                    advance.Add(fullPatternList[x]);
+                    break;
+                case Difficulty.Expert:
+                    expert.Add(fullPatternList[x]);
+                    break;
+            }
+        }
+
+        PatternsRegistry.Add(Difficulty.Starting, starting);
+        PatternsRegistry.Add(Difficulty.Beginner, beginner);
+        PatternsRegistry.Add(Difficulty.Novice, novice);
+        PatternsRegistry.Add(Difficulty.Advance, advance);
+        PatternsRegistry.Add(Difficulty.Expert, expert);
+    }
+}
--- IGME450Project2/Assets/Scripts/GridManager.cs
+++ IGME450Project2/Assets/Scripts/GridManager.cs
@@ -1,14 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
+    public static GridManager Instance;
+
     [SerializeField] private int _width;
     [SerializeField] private int _height;
-    [SerializeField] private Tile _tileprefab;
-    [SerializeField] private Transform _cam;
+
+    [SerializeField] private GameObject _tileprefab;
+
+    private List<List<GameObject>> _tileList = new List<List< GameObject>>();
+
+    public List<List<GameObject>> TileList { get { return _tileList; } }
+
+    public int MapWidth { get { return _width; } }
+
+    public int MapHeight { get { return _height; } }
 
     public int Width => _width;   // Added for boundary access
     public int Height => _height; // Added for boundary access
+
+    public void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        GenerateGrid();
+    }
 
     void GenerateGrid()
     {
@@ -17,17 +46,16 @@ public class GridManager : MonoBehaviour
 
         for (int i = 0; i < _width; i++)
         {
+            List<GameObject> tempList = new List<GameObject>();
             for (int j = 0; j < _height; j++)
             {
                 Vector3 tilePosition = new Vector3(i + xOffset, j + yOffset, 0);
-                Tile spawnTile = Instantiate(_tileprefab, tilePosition, Quaternion.identity);
+                GameObject spawnTile = Instantiate(_tileprefab, tilePosition, Quaternion.identity);
                 spawnTile.name = $"Tile {i} {j}";
+                tempList.Add(spawnTile);
             }
+            _tileList.Add(tempList);
         }
     }
-
-    void Start()
-    {
-        GenerateGrid();
-    }
+    
 }
